56c95d354ed95edbbae0c18d1ae098b4096e7b36
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2024-04-10 21:57:25 +0545

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,128 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+
+  {
+    name: "HTML",
+    Image: "/html.png",
+    width: 90,
+    height: 90,
+  },
+ 
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 90,
+    height: 90,
+  },
+  {
+    name: "Kicad",
+    Image: "/KiCad-1.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "Proteus",
+    Image: "/proteus.png",
+    width: 90,
+    height: 90,
+  },
+  
+  {
+    name: "Tinker Cad",
+    Image: "/tinker.png",
+    width: 90,
+    height: 90,
+  },
+  {
+    name: "PCB Design",
+    Image: "/pcbdd.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "C programming",
+    Image: "/c.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "C++",
+    Image: "/cpp1.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "Python",
+    Image: "/python.png",
+    width: 75,
+    height: 75,
+  },
+
+  {
+    name: "Arduino",
+    Image: "/arduino.png",
+    width: 85,
+    height: 85,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Space Themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
